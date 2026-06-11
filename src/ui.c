4df243c7149540f31f3c07d41bfa866e0add02b8
Yazaaan <ysawalha05@gmail.com>
2026-06-11 19:26:18 +0200

    refractoring: Größere Tasten-Aktionen in Funktionen ausgelagert

--- src/ui.c
+++ src/ui.c
@@ -10,6 +10,12 @@
 
 char global_message[128] = ""; // Globale Varialble für aktuelle Infonachricht
 
+// Vorwärtsdeklarationen für Tasten-Aktionen:
+void ui_action_key_clear(Game_State *game);
+void ui_action_key_scaling_mode(Game_State *game);
+void ui_action_key_save_toggle(Game_State *game);
+void ui_action_key_save_to_slot(Game_State *game, int slot);
+
 // Ist das Termianl besonders klein?
 bool small_terminal_mode(void) {
   return LINES < TERMINAL_MIN_HEIGHT || COLS < TERMINAL_MIN_WIDTH;
@@ -56,7 +62,8 @@ void ui_init(Game_State *game) {
 
   int init_height = get_available_height();
   int init_width = get_available_width();
-  game->universe = get_empty_universe(init_height, init_width, START_VARIABLE_SIZE);
+  game->universe =
+      get_empty_universe(init_height, init_width, START_VARIABLE_SIZE);
 
   // Willkommens-Universum laden
   load_grid(&game->universe, 10);
@@ -103,7 +110,7 @@ void ui_input_process_keyboard(Game_State *game, int input) {
   case 'n':
     if (!game->play)
       time_step(game->universe);
-    set_message("Stepping a single frame");
+    set_message("Stepping to next frame");
     break;
   case 'r':
     fill_universe_random(game->universe, RANDOM_CELL_PROBABILITY);
@@ -111,73 +118,18 @@ void ui_input_process_keyboard(Game_State *game, int input) {
     set_message("The Big Bang!");
     break;
   case 'c':
-    bool var_dim = game->universe->variable_dimension;
-    int new_height, new_width;
-    int ccount = game->universe->cells_alive;
-    int fcount = game->universe->frame_count;
-
-    if (var_dim || ccount + fcount == 0) {
-      new_height = get_available_height();
-      new_width = get_available_width();
-    } else {
-      new_height = game->universe->height;
-      new_width = game->universe->width;
-    }
-
-    if (ccount + fcount == 0) {
-      load_grid(&game->universe, 11);
-      resize_universe(&game->universe, new_height, new_width);
-      game->universe->variable_dimension = true;
-      set_message("You found the Glider Gun!!!");
-    } else {
-      reset_universe(game->universe);
-      set_message("Space for something new!");
-    }
-
-    game->play = false;
+    ui_action_key_clear(game);
     break;
   case 'h':
-    if (game->universe->variable_dimension) {
-      game->universe->variable_dimension = false;
-      set_message("Universe size is now fixed (%d x %d)", game->universe->width,
-                  game->universe->height);
-    } else {
-      game->universe->variable_dimension = true;
-      resize_universe(&game->universe, get_available_height(),
-                      get_available_width());
-
-      set_message("Universe size is now depending on terminal size");
-    }
+    ui_action_key_scaling_mode(game);
     break;
   case 's':
-    game->save_mode = !game->save_mode;
-    if (game->save_mode) {
-      set_message(
-          "Press any number from 0 to 9 to save current universe to slot.");
-
-    } else {
-      set_message(
-          "Load universe from slot by pressing any number from 0 to 9.");
-    }
+    ui_action_key_save_toggle(game);
     break;
   default:
     if (input >= '0' && input <= '9') {
       int slot = input - '0';
-      if (game->save_mode) {
-        if (save_grid(game->universe, slot) == 0) {
-          set_message("Universe saved to slot %d!", slot);
-        } else {
-          set_message("There was an error saving the universe to disk slot %d!",
-                      slot);
-        }
-      } else {
-        if (load_grid(&game->universe, slot) == 0) {
-          set_message("Universe loadeded from slot %d with fixed size!", slot);
-        } else {
-          set_message("There was an error loading the universe from slot %d!",
-                      slot);
-        }
-      }
+      ui_action_key_save_to_slot(game, slot);
     }
     break;
   }
@@ -214,13 +166,13 @@ void ui_input_process_mouse(Game_State *game, MEVENT *mouse_event) {
         click_x < game->universe->width && click_x >= 0) {
 
       // Zustand wechseln
-      bool cell = get_cell_state(game->universe, click_y, click_x);
-      change_cell(game->universe, click_y, click_x, !cell);
+      bool cell_state = get_cell_state(game->universe, click_y, click_x);
+      change_cell(game->universe, click_y, click_x, !cell_state);
 
       game->universe->frame_count = 0;
 
       set_message("Edit mode: changed cell at x:%d, y:%d to %s", click_x,
-                  click_y, (cell) ? "alive" : "dead");
+                  click_y, (!cell_state) ? "alive" : "dead");
     } else {
       set_message("Edit mode: out of bounds at x:%d, y:%d", click_x, click_y);
     }
@@ -251,19 +203,94 @@ void ui_process_input(Game_State *game) {
   ui_draw(game);
 }
 
-// Orchestriert das Zeichnen der Benutzerberfläche
+// Tasten-Aktionen für das Leeren des Felds
+void ui_action_key_clear(Game_State *game) {
+  bool var_dim = game->universe->variable_dimension;
+  int new_height, new_width;
+  int ccount = game->universe->cells_alive;
+  int fcount = game->universe->frame_count;
+
+  // Bedingung ccount + fcount == 0 triggert ein Easteregg, wenn zwei Mal
+  // gecleared wird!
+  if (var_dim || ccount + fcount == 0) {
+    new_height = get_available_height();
+    new_width = get_available_width();
+  } else {
+    new_height = game->universe->height;
+    new_width = game->universe->width;
+  }
+
+  if (ccount + fcount == 0) {
+    load_grid(&game->universe, 11);
+    resize_universe(&game->universe, new_height, new_width);
+    game->universe->variable_dimension = true;
+    set_message("You found the Glider Gun!!!");
+  } else {
+    reset_universe(game->universe);
+    set_message("Space for something new!");
+  }
+
+  game->play = false;
+}
+
+// Tasten-Aktionen für das Umschalten des Skalierungsmodus
+void ui_action_key_scaling_mode(Game_State *game) {
+  if (game->universe->variable_dimension) {
+    game->universe->variable_dimension = false;
+    set_message("Universe size is now fixed (%d x %d)", game->universe->width,
+                game->universe->height);
+  } else {
+    game->universe->variable_dimension = true;
+    resize_universe(&game->universe, get_available_height(),
+                    get_available_width());
+
+    set_message("Universe size is now depending on terminal size");
+  }
+}
+
+// Tasten-Aktionen für das Umschalten des Speichern-/Laden-Modus
+void ui_action_key_save_toggle(Game_State *game) {
+  game->save_mode = !game->save_mode;
+  if (game->save_mode) {
+    set_message(
+        "Press any number from 0 to 9 to save current universe to slot.");
+  } else {
+    set_message("Load universe from slot by pressing any number from 0 to 9.");
+  }
+}
+
+// Tasten-Aktionen für das Speichern des Grids in einen Slot (0 bis 9)
+void ui_action_key_save_to_slot(Game_State *game, int slot) {
+  if (game->save_mode) {
+    if (save_grid(game->universe, slot) == 0) {
+      set_message("Universe saved to slot %d!", slot);
+    } else {
+      set_message("There was an error saving the universe to disk slot %d!",
+                  slot);
+    }
+  } else {
+    if (load_grid(&game->universe, slot) == 0) {
+      set_message("Universe loadeded from slot %d with fixed size!", slot);
+    } else {
+      set_message("There was an error loading the universe from slot %d!",
+                  slot);
+    }
+  }
+}
+
+// Orchestriert das Zeichnen/Rendern der Benutzerberfläche
 void ui_draw(Game_State *game) {
-  // clear();
   erase();
 
-  // Die komplette UI wird nur gezeichnet, wenn das Terminal groß genug ist
+  // Die komplette UI wird nur gezeichnet, wenn das Terminal groß genug ist,
+  // sonst nur das wichtigste
   if (!small_terminal_mode()) {
     print_headline();
     print_controls();
     print_stats(game);
     print_dividers();
     print_universe(game->universe, GRID_START_Y, GRID_START_X, GRID_MARGIN_Y,
-                     GRID_MARGIN_X);
+                   GRID_MARGIN_X);
     print_message(LINES - 1, GRID_START_X, global_message);
   } else {
     print_universe(game->universe, 0, 0, 1, 0);
